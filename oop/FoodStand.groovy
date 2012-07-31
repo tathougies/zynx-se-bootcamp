6db482bdaaab137d50a88da08645ab245e4dd33b
unknown <ZHLOCAL@HBMLOS-DEV6.CompanyNet.org>
2012-07-31 14:06:06 -0700

    Lab 4 code and instructions

--- oop/FoodStand.groovy
+++ oop/FoodStand.groovy
@@ -3,16 +3,20 @@ class FoodStand {
     private foodPrices
     private foodStock
     private money
+    private validate = new Validator()
+    private paymentCalculator
 
     FoodStand(String[] foodNames,
-              Map<String, Money> foodPrices,
-              Map<String, Integer> foodStock,
-              Money money
+               Map<String, Money> foodPrices,
+               Map<String, Integer> foodStock,
+               Money money,
+               ChangeCalculator paymentCalculator
     ) {
         this.foodNames = foodNames
         this.foodPrices = foodPrices
         this.foodStock = foodStock
         this.money = money
+        this.paymentCalculator = paymentCalculator
     }
 
     String[] getFoodNames() {
@@ -23,22 +27,25 @@ class FoodStand {
         return "Food Stand"
     }
 
-    void showPrice(String foodName) {
-        if (foodName in foodNames) {
-            println "${foodName} costs ${foodPrices[foodName]}"
-        }
-        else {
-            println "Sorry, we do not have ${foodName}s here."
-        }
+    private String noFoodMessage(String foodName) {
+        return "Sorry, we do not have ${foodName} here."
     }
 
-    void takeMoney(Money price, Money payment) throws Exception {
-        if (payment >= price) {
-            println "Your change is ${payment - price}."
-            money += price
+    private String outOfStockMessage(String foodName) {
+        return "Sorry, we are out of ${foodName}."
+    }
+
+    private String restockingWithNothingMessage() {
+        return "You need to restock with a positive amount."
+    }
+
+    void showPrice(String foodName) {
+        try {
+            validate.inList(foodName, foodNames, noFoodMessage(foodName))
+            println "${foodName} costs ${foodPrices[foodName]}"
         }
-        else {
-            throw new Exception("Sorry, that is not enough")
+        catch (Exception e) {
+            println e.getMessage()
         }
     }
 
@@ -50,48 +57,45 @@ class FoodStand {
     }
 
     void showAmountOfFood(String foodName) {
-        if (foodName in foodNames) {
+        try {
+            validate.inList(foodName, foodNames, noFoodMessage(foodName))
             println "We have ${foodStock[foodName]} ${foodName}s."
         }
-        else {
-            println "Sorry, we do not have ${foodName}s here."
+        catch (Exception e) {
+            println e.getMessage()
         }
     }
 
     void buyFood(String foodName, Money payment) {
-        if (foodName in foodNames) {
-            if (foodStock[foodName] > 0) {
-                try {
-                    takeMoney(foodPrices[foodName], payment)
-                    println "Here is your ${foodName}"
-                    foodStock[foodName] -= 1
-                }
-                catch (Exception e) {
-                    println e.getMessage()
-                }
-            }
-            else {
-                println "Sorry, we are out of ${foodName}s."
-            }
+        try {
+            validate.inList(foodName, foodNames, noFoodMessage(foodName))
+            validate.isPositive(foodStock[foodName], outOfStockMessage(foodName))
+            def price = foodPrices[foodName]
+            def change = paymentCalculator.calculateChange(payment, price)
+            println "Your change is ${change}."
+            money += price
+            println "Here is your ${foodName}"
+            foodStock[foodName] -= 1
         }
-        else {
-            println "Sorry, we do not have ${foodName}s here."
+        catch (Exception e) {
+            println e.getMessage()
         }
     }
 
     void restockFood(String foodName, Integer newFoodAmount) {
-        if (foodName in foodNames) {
-            if (newFoodAmount >= 0) {
-                foodStock[foodName] += newFoodAmount
-                println "We used to have ${foodStock[foodName] - newFoodAmount} ${foodName}s and now we have ${foodStock[foodName]}"
-            }
-            else {
-                println "You can't restock with a negative amount!"
-            }
+        try {
+            validate.inList(foodName, foodNames, noFoodMessage(foodName))
+            validate.isPositive(newFoodAmount, restockingWithNothingMessage())
+            foodStock[foodName] += newFoodAmount
+            println "We used to have ${foodStock[foodName] - newFoodAmount} ${foodName}s and now we have ${foodStock[foodName]}"
         }
-        else {
-            println "Sorry, we do not have ${foodName}s here."
+        catch (Exception e) {
+            println e.getMessage()
         }
+    }
+
+    void setPaymentCalculator(ChangeCalculator paymentCalculator) {
+        this.paymentCalculator = paymentCalculator
     }
 
     Money getMoney() {
--- oop/TicketBooth.groovy
+++ oop/TicketBooth.groovy
@@ -4,40 +4,51 @@ class TicketBooth {
     private ticketPrice
     private tickets
     private money
+    private validate = new Validator()
+    private paymentCalculator
 
     TicketBooth(String[] movieNames,
-                Map<String, Time> movieTimes,
-                Money ticketPrice,
-                Integer tickets,
-                Money money
+                 Map<String, Time> movieTimes,
+                 Money ticketPrice,
+                 Integer tickets,
+                 Money money,
+                 ChangeCalculator paymentCalculator
     ) {
         this.movieNames = movieNames
         this.movieTimes = movieTimes
         this.ticketPrice = ticketPrice
         this.tickets = tickets
         this.money = money
+        this.paymentCalculator = paymentCalculator
     }
 
     String toString() {
         return "Ticket Booth"
     }
 
-    void showTicketPrice(String movieName) {
-        if (movieName in movieNames) {
-            println "The ticket will cost ${ticketPrice}."
-        }
-        else {
-            println "We are not showing ${movieName} here."
-        }
+    private String noMovieMessage(String movieName) {
+        return "We are not showing ${movieName} here."
     }
 
-    void takeTicketMoney(Money price, Money payment) throws Exception {
-        if (payment >= price) {
-            println "Your change is ${payment - price}."
-            money += price
+    private String noTimeMessage(String movieName, Time movieTime) {
+        return "We are not showing ${movieName} at ${movieTime} here."
+    }
+
+    private String noTicketsMessage() {
+        return "We are out of tickets."
+    }
+
+    private String restockingWithNothingMessage() {
+        return "You need to restock with a positive amount."
+    }
+
+    void showTicketPrice(String movieName) {
+        try {
+            validate.inList(movieName, movieNames, noMovieMessage(movieName))
+            println "The ticket will cost ${ticketPrice}."
         }
-        else {
-            throw new Exception("Sorry, that is not enough")
+        catch (Exception e) {
+            println e.getMessage()
         }
     }
 
@@ -56,39 +67,33 @@ class TicketBooth {
     }
 
     void buyTicket(String movieName, Time movieTime, Money payment) {
-        if (movieName in movieNames) {
-            if (movieTimes[movieName].contains(movieTime)) {
-                if (tickets > 0) {
-                    try {
-                        takeTicketMoney(ticketPrice, payment)
-                        println "Here is your ${movieName} ticket."
-                        tickets -= 1
-                    }
-                    catch (Exception e) {
-                        println e.getMessage()
-                    }
-                }
-                else {
-                    println "Sorry, we are out of tickets for ${movieName}."
-                }
-            }
-            else {
-                println "We are not showing ${movieName} at ${movieTime}."
-            }
+        try{
+            validate.inList(movieName, movieNames, noMovieMessage(movieName))
+            validate.inList(movieTime, movieTimes[movieName], noTimeMessage(movieName, movieTime))
+            validate.isPositive(tickets, noTicketsMessage())
+            def change = paymentCalculator.calculateChange(payment, ticketPrice)
+            println "Your change is ${change}."
+            money += ticketPrice
+            println "Here is your ${movieName} ticket."
+            tickets -= 1
         }
-        else {
-            println "We are not showing ${movieName} here."
+        catch (Exception e) {
+            println e.getMessage()
         }
     }
 
     void restockTickets(Integer newTicketAmount) {
-        if (newTicketAmount >= 0) {
-            tickets += newTicketAmount
+        try {
+            validate.isPositive(newTicketAmount, restockingWithNothingMessage())
             println "We used to have ${tickets - newTicketAmount} tickets and now we have ${tickets}"
         }
-        else {
-            println "You can't restock with a negative amount!"
+        catch (Exception e) {
+            println e.getMessage()
         }
+    }
+
+    void setPaymentCalculator(ChangeCalculator paymentCalculator) {
+        this.paymentCalculator = paymentCalculator
     }
 
     Money getMoney() {
